44d889c2c025a6ef71deb92ebc7ee3b0be7895a7
GABRIEL MATHEUS ALVES DO NASCIMENTO SOUSA <gabriel.mansousa@senacsp.edu.br>
2024-05-07 17:15:29 -0300

    sql comandos

--- dbLoja.sql
+++ dbLoja.sql
@@ -11,15 +11,11 @@ use dbLoja;
 show databases;
 
 -- criando as tabelas no bd
-create table tbUsuarios(
-codUsu int,
-nome varchar(50),
-senha varchar(20)
-);
+
 
 create table tbFuncionarios(
-codFunc int auto_increment, 
-nome varchar(100),
+codFunc int not null auto_increment, 
+nome varchar(100) not null,
 email varchar(100),
 cpf char(14) not null unique,
 telCel char(10),
@@ -31,6 +27,44 @@ cidade varchar(100),
 estado char(2),
 primary key(codFunc)
 );
+
+create table tbFornecedor(
+	codForn int not null auto_increment,
+	nome varchar(100),
+	email varchar(100),
+	cnpj char(16) not null unique,
+	primary key(codForn)
+);
+
+create table tbClientes(
+	codCli int not null auto_increment,
+	nome varchar(100),
+	email char(100),
+	cpf char(14) not null unique,
+	primary key(codCli) 
+);
+
+create table tbUsuarios(
+codUsu int auto_increment,
+nome varchar(50),
+senha varchar(20),
+codFunc int not null,
+primary key(codUsu),
+foreign key(codFunc) references tbFuncionarios(codFunc) 
+);
+
+create table tbProdutos(
+	codProd int not null auto_increment,
+	descricao varchar(100),
+    valor decimal(9,2),
+	quantidade int,
+	data date,
+	hora time,
+	codForn int not null,
+	primary key(codProd),
+	foreign key(codForn) references tbFornecedores(codForn)
+);
+
 
 
 
@@ -44,18 +78,29 @@ desc tbFuncionarios;
 
 -- inserindo registros nas tabelas
 
-insert into tbUsuarios(codUsu,nome,senha)
-	values(1,'maria.gabrielly','deximbranco');
 
-insert into tbFuncionarios(codFunc,nome,email,cpf,telCel)
-	values(10,'Maria Gagrielly Bejamim',
+
+insert into tbFuncionarios(nome,email,cpf,telCel, logradouro, numero, cep, bairro, cidade, estado)
+	values('Maria Gagrielly Bejamim',
 		'maria.gbenjamim@hotmail.com','478.856.875-85',
-		'98523-8547');
+		'98523-8547','Rua Dr. Antonio Bento',101,'04750-000','Santo Amaro', 'Sao Paulo','SP');
 
+		insert into tbFuncionarios(nome,email,cpf,telCel, logradouro, numero, cep, bairro, cidade, estado)
+	values('Ana Frios Delgado Matarazzo',
+		'ana.fdMatarazzo@hotmail.com','4278.856.875-85',
+		'02456-000','Santo Amaro',101,'04750-000','Santo Amaro', 'Sao Paulo','SP');
+
+		
+insert into tbUsuarios(nome,senha,codFunc)
+	values('maria.gabrielly','deximbranco',1);
+	insert into tbUsuarios(nome,senha,codFunc)
+	values('ana.fdmatarazzo','123456',2);
 -- visualizando os registros nos campos das tabelas
 
-select * from tbUsuarios;
 select * from tbFuncionarios;
+select * from tbUsuarios;
+select * from tbProdutos;
+
 
 
 
